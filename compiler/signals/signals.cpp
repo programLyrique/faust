4ada9a355dca14c911651e968c4d490b56759375
Yann Orlarey <orlarey@gmail.com>
2018-08-01 16:55:22 +0200

    Soundfile primitive has now two inputs. The first one is the part number (0,1,...,n-1) were n is the number of concatenated files. The second one is the read index into the current part.

--- compiler/signals/signals.cpp
+++ compiler/signals/signals.cpp
@@ -610,9 +610,9 @@ Tree sigSoundfileChannels(Tree sf)
 {
     return tree(gGlobal->SIGSOUNDFILECHANNELS, sf);
 }
-Tree sigSoundfileBuffer(Tree sf, Tree chan, Tree ridx)
+Tree sigSoundfileBuffer(Tree sf, Tree chan, Tree fpart, Tree ridx)
 {
-    return tree(gGlobal->SIGSOUNDFILEBUFFER, sf, chan, ridx);
+    return tree(gGlobal->SIGSOUNDFILEBUFFER, sf, chan, fpart, ridx);
 }
 
 bool isSigSoundfile(Tree s, Tree& label)
@@ -631,9 +631,9 @@ bool isSigSoundfileChannels(Tree s, Tree& sf)
 {
     return isTree(s, gGlobal->SIGSOUNDFILECHANNELS, sf);
 }
-bool isSigSoundfileBuffer(Tree s, Tree& sf, Tree& chan, Tree& ridx)
+bool isSigSoundfileBuffer(Tree s, Tree& sf, Tree& chan, Tree& fpart, Tree& ridx)
 {
-    return isTree(s, gGlobal->SIGSOUNDFILEBUFFER, sf, chan, ridx);
+    return isTree(s, gGlobal->SIGSOUNDFILEBUFFER, sf, chan, fpart, ridx);
 }
 /*****************************************************************************
                              matrix extension
